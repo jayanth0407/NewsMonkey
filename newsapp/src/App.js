e7923d65a9ec4f85c14948bc13bdd28d553e555d
jayanth0407 <jayanthkumarjayanth477@gmail.com>
2025-07-22 23:06:55 +0530

    first commit

---
+++ newsapp/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+
+
+
+export default class App extends Component { a='jayanth'
+  render() {
+    
+    return (
+      <div>
+       <Navbar/>
+       <News pagesize={6}/>
+      </div>
+    )
+  }
+}
